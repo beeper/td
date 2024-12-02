47fe719017ba9269d450ec03d0d1a6d02df0e5a4
Sumner Evans <sumner.evans@automattic.com>
2024-12-02 11:47:04 -0700

    auth: handle AUTH_KEY_DUPLICATED the same as 401s
    Signed-off-by: Sumner Evans <sumner.evans@automattic.com>

--- telegram/auth/auth.go
+++ telegram/auth/auth.go
@@ -12,9 +12,12 @@ func IsKeyUnregistered(err error) bool {
 	return tgerr.Is(err, "AUTH_KEY_UNREGISTERED")
 }
 
-// IsUnauthorized reports whether err is 401 UNAUTHORIZED.
+// IsUnauthorized reports whether err is any 401 UNAUTHORIZED or is a 406
+// NOT_ACCEPTABLE with AUTH_KEY_DUPLICATED.
 //
 // https://core.telegram.org/api/errors#401-unauthorized
+// https://core.telegram.org/api/errors#406-not-acceptable
 func IsUnauthorized(err error) bool {
-	return tgerr.IsCode(err, 401)
+	return tgerr.IsCode(err, 401) ||
+		(tgerr.IsCode(err, 406) && tgerr.Is(err, "AUTH_KEY_DUPLICATED"))
 }
